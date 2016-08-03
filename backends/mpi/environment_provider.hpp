a54d031f124f32bb4b035e7dc74fb316fc70aec3
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2016-08-03 17:38:21 +0200

    Add MPI file structure

---
+++ backends/mpi/environment_provider.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "backend.hpp"
+
+namespace bulk {
+namespace mpi {
+
+class provider {
+  public:
+    provider();
+    ~provider();
+
+  private:
+};
+
+} // namespace mpi
+} // namespace bulk
