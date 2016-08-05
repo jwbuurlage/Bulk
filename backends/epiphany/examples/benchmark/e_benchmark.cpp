d51760fc0061d847ceb9618246b1d05dc0738982
Tom Bannink <tombannink@gmail.com>
2016-08-05 00:26:59 +0000

    Fix barrier benchmark

--- backends/epiphany/examples/benchmark/e_benchmark.cpp
+++ backends/epiphany/examples/benchmark/e_benchmark.cpp
@@ -4,18 +4,25 @@
 using bulk::epiphany::world;
 
 int main() {
-    //int s = world.processor_id();
+    int s = world.processor_id();
+    int p = world.active_processors();
     world.sync();
 
-    world.barrier();
     get_raw_time();
     get_raw_time();
-    world.barrier();
     unsigned int t1 = get_raw_time();
 
-    bulk::epiphany::print("Barrier time: %d", t1);
+    bulk::epiphany::print("Nothing time: %d", t1);
 
-    constexpr int nmax = 10;
+    world.barrier();
+    get_raw_time();
+    get_raw_time();
+    world.barrier();
+    unsigned int t2 = get_raw_time();
+
+    bulk::epiphany::print("Barrier time: %d", t2);
+
+    constexpr int nmax = 11;
     unsigned int* times = new unsigned int[nmax];
     for (int i = 0; i < nmax; ++i) {
         get_raw_time();
@@ -25,8 +32,13 @@ int main() {
         times[i] = t;
     }
 
-    for (int i = 0; i < nmax; ++i) {
-        bulk::epiphany::print("(%d,%d)", i, times[i]);
+    for (int k = 0; k < p; k++) {
+        world.barrier();
+        if (k != s)
+            continue;
+        for (int i = 0; i < nmax; ++i) {
+            bulk::epiphany::print("(%d,%d)", i, times[i]);
+        }
     }
 
     delete[] times;
--- backends/epiphany/examples/benchmark/host_benchmark.cpp
+++ backends/epiphany/examples/benchmark/host_benchmark.cpp
@@ -8,27 +8,7 @@ int main(int argc, char** argv) {
     if (!env.provider().is_valid())
         return 1;
 
-    int count = 32;
-    std::vector<std::vector<int>> data(env.available_processors());
-
-    for (int i = 0; i < env.available_processors(); i++) {
-        data[i].resize(count);
-        for (int j = 0; j < count; j++)
-            data[i][j] = i + j;
-    }
-
-    for (int i = 0; i < env.available_processors(); i++)
-        env.provider().create_stream(&data[i][0], count * sizeof(int),
-                                     count * sizeof(int));
-
-    env.spawn(env.available_processors(), "e_basic.elf");
-
-    for (int i = 0; i < env.available_processors(); i++) {
-        std::cout << "Result from processor " << i << ":\n";
-        for (int j = 0; j < count; j++)
-            std::cout << j << ' ';
-        std::cout << '\n';
-    }
+    env.spawn(env.available_processors(), "e_benchmark.elf");
 
     return 0;
 }
