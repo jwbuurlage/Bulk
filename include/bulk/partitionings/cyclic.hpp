877648ff7657bbd844f04910011c2ab732004207
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2017-01-03 17:40:12 +0100

    Refactor partitioning system

---
+++ include/bulk/partitionings/cyclic.hpp
@@ -0,0 +1,71 @@
+#include "partitioning.hpp"
+
+namespace bulk {
+
+/**
+ * This cyclic partitioning is an initial draft of what will eventually part of
+ * a bigger class of partitionings.
+ *
+ * In particular, block distributions are nearly identical, except for indexing
+ * functions being slightly different, make common base class for this.
+ *
+ * Future challenges include how to coalesce lookups for distributed owner/index
+ * tables.
+ */
+template <int D, int G = D>
+class cyclic_partitioning : public multi_partitioning<D, G> {
+   public:
+    /**
+     * Constructs a cyclic partitioning in nD.
+     *
+     * `grid`: the number of processors in each dimension
+     * `data_size`: the global number of processors along each axis
+     */
+    cyclic_partitioning(bulk::world& world, index_type<D> data_size,
+                        index_type<G> grid)
+        : multi_partitioning<D, G>(world, data_size, grid) {
+        static_assert(G <= D,
+                      "Dimensionality of the data should be larger or equal to "
+                      "that of the processor grid.");
+    }
+
+    /** Compute the local indices of a element using its global indices */
+    index_type<D> global_to_local(index_type<D> index) override final {
+        for (int d = 0; d < G; ++d) {
+            index[d] = index[d] / this->grid_size_[d];
+        }
+        return index;
+    }
+
+    /** Local to global */
+    index_type<D> local_to_global(index_type<D> xs,
+                                  index_type<G> processor) override final {
+        // FIXME implement
+        return index_type<D>{};
+    }
+
+    /** The total number of elements along each axis on the processor index with
+     * `idxs...` */
+    index_type<D> local_size(index_type<G> idxs) override final {
+        index_type<D> size;
+        for (int dim = 0; dim < G; ++dim) {
+            size[dim] = (this->global_size_[dim] + this->grid_size_[dim] - idxs[dim] - 1) /
+                        this->grid_size_[dim];
+        }
+        for (int dim = G; dim < D; ++dim) {
+            size[dim] = this->global_size_[dim];
+        }
+        return size;
+    }
+
+    /** Cyclic in first 'G' dimensions. */
+    index_type<G> grid_owner(index_type<D> xs) override final {
+        index_type<G> result;
+        for (int d = 0; d < G; ++d) {
+            result[d] = xs[d] % this->grid_size_[d];
+        }
+        return result;
+    }
+};
+
+}  // namespace bulk
--- test/partitioning.cpp
+++ test/partitioning.cpp
@@ -12,35 +12,37 @@ void test_partitioning() {
         BULK_REQUIRE(N * N == p);
 
         BULK_SECTION("Cyclic partitioning to 1D") {
-            auto part =
-                bulk::cyclic_partitioning<2, 1>({p}, {5 * p * N, 5 * p * N});
-            BULK_CHECK_ONCE(part.owner({p + 2, 3})[0] == 2,
+            auto part = bulk::cyclic_partitioning<2, 1>(
+                world, {5 * p * N, 5 * p * N}, {p});
+            BULK_CHECK_ONCE(part.owner({p + 2, 3}) == 2,
                             "compute correctly the cyclic from 2 -> 1 dim");
-            BULK_CHECK_ONCE(part.local_extent({s})[0] == 5 * N,
+            BULK_CHECK_ONCE(part.local_size({s})[0] == 5 * N,
                             "compute correctly the extent in cyclic dim");
-            BULK_CHECK_ONCE(part.local_extent({s})[1] == 5 * p * N,
+            BULK_CHECK_ONCE(part.local_size({s})[1] == 5 * p * N,
                             "compute correctly the extent in non-cyclic dim");
-            BULK_CHECK_ONCE(part.local_index({p + 2, 3})[0] == 1,
+            BULK_CHECK_ONCE(part.global_to_local({p + 2, 3})[0] == 1,
                             "compute correctly the local index");
         }
 
         BULK_SECTION("Cyclic partitioning") {
-            auto part = bulk::cyclic_partitioning<2>({N, N}, {10 * N, 10 * N});
-            BULK_CHECK_ONCE(part.owner({4, 3})[0] == 4 % N,
+            auto part = bulk::cyclic_partitioning<2, 2>(world, {10 * N, 10 * N},
+                                                        {N, N});
+            BULK_CHECK_ONCE(part.grid_owner({4, 3})[0] == 4 % N,
                             "compute correctly the cyclic owner");
-            BULK_CHECK_ONCE(part.local_extent({s % N, s / N})[0] == 10,
-                            "compute correctly the cyclic extent");
-            BULK_CHECK_ONCE(part.local_index({4, 3})[0] == 4 / N,
+            BULK_CHECK_ONCE(part.local_size({s % N, s / N})[0] == 10,
+                            "compute correctly the cyclic size");
+            BULK_CHECK_ONCE(part.global_to_local({4, 3})[0] == 4 / N,
                             "compute correctly the cyclic local index");
         }
 
         BULK_SECTION("Block partitioning") {
-            auto part = bulk::block_partitioning<2>({N, N}, {10 * N, 10 * N});
-            BULK_CHECK_ONCE(part.owner({2 * 10 + 3, 3})[0] == 2,
+            auto part =
+                bulk::block_partitioning<2, 2>(world, {10 * N, 10 * N}, {N, N});
+            BULK_CHECK_ONCE(part.grid_owner({2 * 10 + 3, 3})[0] == 2,
                             "compute correctly the block owner");
-            BULK_CHECK_ONCE(part.local_extent({s % N, s / N})[0] == 10,
+            BULK_CHECK_ONCE(part.local_size({s % N, s / N})[0] == 10,
                             "compute correctly the block extent");
-            BULK_CHECK_ONCE(part.local_index({3, 12})[1] == 2,
+            BULK_CHECK_ONCE(part.global_to_local({3, 12})[1] == 2,
                             "compute correctly the block index");
             BULK_CHECK_ONCE(part.origin(0)[0] == 0,
                             "compute correctly the block origin (0)");
@@ -58,9 +60,9 @@ void test_partitioning() {
             tree.add(root, dir::right, bulk::split{1, 5});
 
             auto part =
-                bulk::binary_partitioning<2>(4, {10, 10}, std::move(tree));
+                bulk::tree_partitioning<2>(world, {10, 10}, 4, std::move(tree));
 
-            BULK_CHECK_ONCE((part.local_extent({0}) == std::array<int, 2>{5, 5}),
+            BULK_CHECK_ONCE((part.local_size({0}) == std::array<int, 2>{5, 5}),
                             "extent of bspart are correct");
 
             BULK_CHECK_ONCE((part.owner({1, 1}) == part.owner({2, 2})),
@@ -76,29 +78,28 @@ void test_partitioning() {
             BULK_CHECK_ONCE((part.origin(3) == std::array<int, 2>{5, 5}),
                             "assign correct origin bspart (3)");
 
-            BULK_CHECK_ONCE((part.local_index({6, 6}) == std::array<int, 2>{1, 1}),
-                            "assign correct origin bspart");
-
+            BULK_CHECK_ONCE(
+                (part.global_to_local({6, 6}) == std::array<int, 2>{1, 1}),
+                "assign correct origin bspart");
         }
 
         BULK_SECTION("Partitioned array") {
-            auto part = bulk::cyclic_partitioning<2>({N, N}, {200, 200});
-            auto xs =
-                bulk::partitioned_array<int, 2>(world, part);
+            auto part = bulk::cyclic_partitioning<2>(world, {200, 200}, {N, N});
+            auto xs = bulk::partitioned_array<int, 2, 2>(world, part);
 
-            xs.local(0, 0) = s;
-            xs.local(1, 1) = s + 1;
+            xs.local({0, 0}) = s;
+            xs.local({1, 1}) = s + 1;
 
-            auto glob = xs.global(1, 1).get();
+            auto glob = xs.global({1, 1}).get();
             world.sync();
 
             BULK_CHECK_ONCE(glob.value() == N + 1, "obtain remote value");
-            BULK_CHECK_ONCE(xs.local(1, 1) == s + 1, "obtain local value");
+            BULK_CHECK_ONCE(xs.local({1, 1}) == s + 1, "obtain local value");
 
-            xs.global(1, 1) = 1234;
+            xs.global({1, 1}) = 1234;
             world.sync();
 
-            glob = xs.global(1, 1).get();
+            glob = xs.global({1, 1}).get();
             world.sync();
 
             BULK_CHECK_ONCE(glob.value() == 1234, "put remote value");
